f8f7ff333063fb881ea8ac36faabe053b3cf09f9
Aatmik <39591056+Aatmik-Dahal@users.noreply.github.com>
2020-08-13 19:09:45 +0545

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import logging
 logger = logging.getLogger("Testlogger")#this line is important
 logging.basicConfig(level=logging.DEBUG)#this line is important too
 
-logger.info("this is not showing up ")
+logger.info("this is showing up ")#info shows up as Debug is the smallest 
 logger.warning("This is a warning")
 logger.error("This is a error i would say")
 logger.critical("Idk this is a critical messege")
